20215a2e7a3e91fe15c2cf61c867aa74ea2fb77f
Konstantin <yakimtsev.konstantin@gmail.com>
2026-03-02 22:01:24 +0500

    1 hour

---
+++ js/data.js
@@ -0,0 +1,122 @@
+const products = [
+    {
+        id: 1,
+        article: "4607",
+        title: "Угловая бежевая кухня на заказ с матовыми фасадами в современном стиле «Нарцисс»",
+        images: [
+            "images/kitchen-1-1.png",
+            "images/kitchen-1-2.png",
+            "images/kitchen-1-3.png",
+            "images/kitchen-1-4.png",
+            "images/kitchen-1-5.png",
+            "images/kitchen-1-6.png"
+        ],
+        modifications: [
+            { name: "ПВХ", price: "475 000 ₽", meter: "27 000 ₽", isActive: false },
+            { name: "Эмаль", price: "515 700 ₽", meter: "30 000 ₽", isActive: true },
+            { name: "Пластик", price: "548 000 ₽", meter: "32 000 ₽", isActive: false },
+            { name: "Массив", price: "890 000 ₽", meter: "52 000 ₽", isActive: false }
+        ],
+        likes: 57
+    },
+    {
+        id: 2,
+        article: "4608",
+        title: "Прямая белая кухня с глянцевыми фасадами в стиле минимализм",
+        images: [
+            "images/kitchen-1-1.png",
+            "images/kitchen-1-2.png",
+            "images/kitchen-1-3.png",
+            "images/kitchen-1-4.png",
+            "images/kitchen-1-5.png",
+            "images/kitchen-1-6.png"
+        ],
+        modifications: [
+            { name: "ПВХ", price: "389 000 ₽", meter: "23 000 ₽", isActive: false },
+            { name: "Эмаль", price: "425 000 ₽", meter: "25 000 ₽", isActive: true },
+            { name: "Пластик", price: "458 000 ₽", meter: "27 000 ₽", isActive: false },
+            { name: "Массив", price: "720 000 ₽", meter: "45 000 ₽", isActive: false }
+        ],
+        likes: 34
+    },
+    {
+        id: 3,
+        article: "4609",
+        title: "Угловая кухня в скандинавском стиле с деревянными фасадами",
+        images: [
+            "images/kitchen-1-1.png",
+            "images/kitchen-1-2.png",
+            "images/kitchen-1-3.png",
+            "images/kitchen-1-4.png",
+            "images/kitchen-1-5.png",
+            "images/kitchen-1-6.png"
+        ],
+        modifications: [
+            { name: "ПВХ", price: "520 000 ₽", meter: "31 000 ₽", isActive: false },
+            { name: "Эмаль", price: "567 000 ₽", meter: "34 000 ₽", isActive: true },
+            { name: "Пластик", price: "599 000 ₽", meter: "36 000 ₽", isActive: false },
+            { name: "Массив", price: "945 000 ₽", meter: "58 000 ₽", isActive: false }
+        ],
+        likes: 89
+    },
+    {
+        id: 4,
+        article: "4610",
+        title: "Модульная кухня в стиле лофт с серыми матовыми фасадами",
+        images: [
+            "images/kitchen-1-1.png",
+            "images/kitchen-1-2.png",
+            "images/kitchen-1-3.png",
+            "images/kitchen-1-4.png",
+            "images/kitchen-1-5.png",
+            "images/kitchen-1-6.png"
+        ],
+        modifications: [
+            { name: "ПВХ", price: "410 000 ₽", meter: "24 000 ₽", isActive: true },
+            { name: "Эмаль", price: "445 000 ₽", meter: "26 000 ₽", isActive: false },
+            { name: "Пластик", price: "479 000 ₽", meter: "28 000 ₽", isActive: false },
+            { name: "Массив", price: "760 000 ₽", meter: "47 000 ₽", isActive: false }
+        ],
+        likes: 42
+    },
+    {
+        id: 5,
+        article: "4611",
+        title: "П-образная кухня с островом в классическом стиле",
+        images: [
+            "images/kitchen-1-1.png",
+            "images/kitchen-1-2.png",
+            "images/kitchen-1-3.png",
+            "images/kitchen-1-4.png",
+            "images/kitchen-1-5.png",
+            "images/kitchen-1-6.png"
+        ],
+        modifications: [
+            { name: "ПВХ", price: "612 000 ₽", meter: "38 000 ₽", isActive: false },
+            { name: "Эмаль", price: "678 000 ₽", meter: "42 000 ₽", isActive: false },
+            { name: "Пластик", price: "715 000 ₽", meter: "44 000 ₽", isActive: true },
+            { name: "Массив", price: "1 120 000 ₽", meter: "70 000 ₽", isActive: false }
+        ],
+        likes: 123
+    },
+    {
+        id: 6,
+        article: "4612",
+        title: "Минималистичная кухня в японском стиле с тёмными фасадами",
+        images: [
+            "images/kitchen-1-1.png",
+            "images/kitchen-1-2.png",
+            "images/kitchen-1-3.png",
+            "images/kitchen-1-4.png",
+            "images/kitchen-1-5.png",
+            "images/kitchen-1-6.png"
+        ],
+        modifications: [
+            { name: "ПВХ", price: "490 000 ₽", meter: "29 000 ₽", isActive: false },
+            { name: "Эмаль", price: "535 000 ₽", meter: "31 000 ₽", isActive: false },
+            { name: "Пластик", price: "568 000 ₽", meter: "33 000 ₽", isActive: true },
+            { name: "Массив", price: "890 000 ₽", meter: "55 000 ₽", isActive: false }
+        ],
+        likes: 67
+    }
+];
